9c3c0ce9d7c55f9344bac337ccfd840ba2a83d86
nano <nano@nanonoMacBook-Air.local>
2023-06-08 09:40:19 +0900

    assetsの追加、themeの変更

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '648096fee289a522a7a5';
+  static const String projectId = '6480828188a64f8f1e72';
+  static const String endPoint = 'http://localhost:80/v1';
+}
---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+export "./appwrite_constants.dart";
+export './assets_constants.dart';
+
+//import "appwrite_constants.dart"
+//import "assets_constants.dart"
+
+//import "constants.dart"
