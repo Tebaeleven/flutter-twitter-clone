9cf62bcfa2e4b9dc0b9e7422284d89f8ac6782f8
nano <nano@nanonoMacBook-Air.local>
2023-06-08 22:02:58 +0900

    user登録ができるように

--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_twitter_clone/common/rounded_small_button.dart';
 import 'package:flutter_twitter_clone/constants/constants.dart';
+import 'package:flutter_twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:flutter_twitter_clone/features/auth/view/login_view.dart';
 import 'package:flutter_twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:flutter_twitter_clone/theme/pallete.dart';
 
-class SignUpView extends StatefulWidget {
-    static route() => MaterialPageRoute(
+class SignUpView extends ConsumerStatefulWidget {
+  static route() => MaterialPageRoute(
         builder: (context) => const SignUpView(),
       );
   const SignUpView({super.key});
 
   @override
-  State<SignUpView> createState() => _SignUpViewState();
+  ConsumerState<SignUpView> createState() => _SignUpViewState();
 }
 
-class _SignUpViewState extends State<SignUpView> {
+class _SignUpViewState extends ConsumerState<SignUpView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  void onSignUp() {
+    ref.read(AuthControllerProvider.notifier).signUp(
+      email: emailController.text,
+      password: passwordController.text,
+      context: context
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +69,7 @@ class _SignUpViewState extends State<SignUpView> {
                 Align(
                   alignment: Alignment.topRight,
                   child: RoundedSmallButton(
-                    onTap: () {},
+                    onTap: onSignUp,
                     label: "Done",
                   ),
                 ),
